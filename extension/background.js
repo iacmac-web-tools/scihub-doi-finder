e919dbd987c21851fffb23c6978d08aa3c73d153
Ivan.Trushin <rymbln@gmail.com>
2021-02-04 02:25:14 +0300

    Link parsing improvement

--- extension/background.js
+++ extension/background.js
@@ -1,25 +1,43 @@
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.query({
-		currentWindow: true,
-		active: true
-	}, function(tab) {
-		chrome.tabs.create({
-			"url": localStorage.sciHubUrl || "https://sci-hub.do/"
-		});
-	});
+chrome.browserAction.onClicked.addListener(function () {
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true,
+    },
+    function (tab) {
+      chrome.tabs.create({
+        url: localStorage.sciHubUrl || "https://sci-hub.do/",
+      });
+    }
+  );
 });
 
-function searchTextOnSciHub(doi){
-	// https://mg.scihub.ltd/10.1126/science.aax3872
-	var url = localStorage.sciHubUrl || "https://sci-hub.do/";
-	var myQuery = encodeURI(url + doi.selectionText.trim());
-	chrome.tabs.create({
-		url: myQuery
-	});
+function searchTextOnSciHub(doi) {
+  var replaces = [
+    "https://doi.org/",
+    "http://doi.org/",
+    "doi.org/",
+    "oi.org/",
+    "i.org/",
+    ".org/",
+    "DOI: ",
+    "DOI:",
+    "DOI",
+  ];
+  doiText = doi.selectionText.trim();
+  for (var i = 0; i < replaces.length; i++) {
+    doiText = doiText.replace(replaces[i], "");
+  }
+  // https://mg.scihub.ltd/10.1126/science.aax3872
+  var url = localStorage.sciHubUrl || "https://sci-hub.do/";
+  var myQuery = encodeURI(url + doiText.trim());
+  chrome.tabs.create({
+    url: myQuery,
+  });
 }
 
 chrome.contextMenus.create({
-	title: 'Open DOI: %s on Sci-Hub',
-	contexts: ['selection'],
-	onclick: searchTextOnSciHub
+  title: "Open DOI: %s on Sci-Hub",
+  contexts: ["selection"],
+  onclick: searchTextOnSciHub,
 });
